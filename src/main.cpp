09a266ba1b12e781c9270a9fe16b79aec7fe70a7
Clayton Herbst <herbsca1@gmail.com>
2022-03-07 18:29:13 +0800

    Set clearing color buffer

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,14 @@
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
     glViewport(0, 0, width, height);
-}  
+}
+
+
+void processInput(GLFWwindow *window)
+{
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
 
 
 int main(int argc, char *argv[])
@@ -45,6 +52,14 @@ int main(int argc, char *argv[])
 
     while (!glfwWindowShouldClose(window))
     {
+        // input
+        processInput(window);
+        
+        // rendering
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        
+        // swap buffers 
         glfwSwapBuffers(window);
         glfwPollEvents(); 
     }
